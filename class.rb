540a49979dc2b83f5b33937b47d842e095f54a14
Martha Czernuszenko <mvczer@gmail.com>
2018-08-10 14:55:24 +0000

    automatically backed up by learn

--- class.rb
+++ class.rb
@@ -1,10 +1,11 @@
-def Kitties
+class Kitties
   
   def initialize(name, breed = "mutt")
     @name = name
     @breed = breed
     puts "My new #{@breed} kitten is named #{@name}"
    end
+   
 end 
 
 gucci = Kitties.new("Gucci", "tabby" )
